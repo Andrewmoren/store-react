8889b8a75a0c30c16ebc1d5f1aaf82f7c5c4adcf
andrew <andrewmoren93@gmail.com>
2022-10-11 16:23:19 +0300

    add route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Cart from "./pages/Cart";
 
 import "./scss/app.scss";
 
@@ -12,7 +14,11 @@ function App() {
       <Header />
       <div className="content">
         <div className="container">
-          <NotFound />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cart = () => {
+  return <h1>Basket</h1>;
+};
+
+export default Cart;
