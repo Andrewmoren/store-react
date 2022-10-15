553708190009cb5405894e269e355477ff47a48e
andrew <andrewmoren93@gmail.com>
2022-10-15 14:17:26 +0300

    add search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import logoSvg from "../assets/img/pizza-logo.svg";
+import Search from "./Search/Search";
 
 const Header = () => {
   return (
@@ -14,6 +15,7 @@ const Header = () => {
             </div>
           </div>
         </Link>
+        <Search />
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
             <span>520 ₽</span>
