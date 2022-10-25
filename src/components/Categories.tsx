4b686d8200004f9c5ce291a54929dc2b5fc7cd73
andrew <andrewmoren93@gmail.com>
2022-10-25 22:17:27 +0300

    refixed Categories, CartItem, NotFoundBLock, Pagination, Skeleton

---
+++ src/components/Categories.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+type CategoriesProps = {
+  value: number;
+  onChangeCategory: any
+
+}
+
+const Categories: React.FC<CategoriesProps> = ({ value, onChangeCategory }) => {
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((cat, i) => (
+          <li
+            key={i}
+            onClick={() => onChangeCategory(i)}
+            className={value === i ? "active" : ""}
+          >
+            {cat}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
