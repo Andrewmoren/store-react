cac5e1279c170eed61fcfc76124b2fa234bf72e2
andrew <andrewmoren93@gmail.com>
2022-11-01 13:40:05 +0300

    start Code Splitting: add & fixed in chunk a Cart component 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-// import Cart from "./pages/Cart";
-import FullPizza from "./pages/FullPizza";
 
 import "./scss/app.scss";
 import MainLayout from "./layouts/MainLayout";
 
-const Cart = React.lazy(() => import('./pages/Cart'));
+const Cart = React.lazy(() => import( /* webpackChunkName: "Cart" */ './pages/Cart'));
+const FullPizza = React.lazy(() => import( /* webpackChunkName: "FullPizza" */'./pages/FullPizza'));
+const NotFound  = React.lazy(() => import( /* webpackChunkName: "NotFound" */'./pages/NotFound'));
 
 function App() {
   return (
@@ -19,8 +18,12 @@ function App() {
         <Route path="cart" element={<Suspense fallback={<div>Loading cart...</div>}>
           <Cart />
           </Suspense>} />
-        <Route path="pizza/:id" element={<FullPizza />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="pizza/:id" element={<Suspense fallback={<div>Loading FullPizza...</div>}>
+          <FullPizza />
+        </Suspense>} />
+        <Route path="*" element={<Suspense fallback={<div>Loading...</div>}>
+          <NotFound />
+          </Suspense>} />
       </Route>
     </Routes>
   );
